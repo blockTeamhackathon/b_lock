8b7e3c87f29962576b05ca2ff6b9a46ea9d322fd
amine <amine@DESKTOP-PIMQCD1>
2017-01-28 21:16:08 +0100

    Clean imports

--- BLockWebApp/src/main/java/com/hackathon/block/model/Lock.java
+++ BLockWebApp/src/main/java/com/hackathon/block/model/Lock.java
@@ -1,14 +1,10 @@
 package com.hackathon.block.model;
 
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 
--- BLockWebApp/src/main/java/com/hackathon/block/repository/UserRepository.java
+++ BLockWebApp/src/main/java/com/hackathon/block/repository/UserRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import com.hackathon.block.model.Transaction;
 import com.hackathon.block.model.User;
 
 @RepositoryRestResource
