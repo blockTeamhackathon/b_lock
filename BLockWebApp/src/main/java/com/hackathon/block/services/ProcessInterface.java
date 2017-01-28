f84524440e49f771c868cdbb936996b0f6f69c8c
Richard Ulrich <richi@paraeasy.ch>
2017-01-28 21:07:11 +0100

    adding txid

--- BLockWebApp/src/main/java/com/hackathon/block/services/ProcessInterface.java
+++ BLockWebApp/src/main/java/com/hackathon/block/services/ProcessInterface.java
@@ -1,14 +1,14 @@
 public interface ProcessInterface {
 
-  public String startTransaction(String json);
+  public String startTransaction(String txid, String json);
 
-  public String lock(String json);
+  public String lock(String txid, String json);
 
-  public String getTransaction(String json);
+  public String getTransaction(String txid, String json);
 
-  public String unlock(String json);
+  public String unlock(String txid, String json);
 
-  public String endTransaction(String json);
+  public String endTransaction(String txid, String json);
 
-  public int getTransactionCount(String id);
+  public int getTransactionCount(String txid, String id);
 }
