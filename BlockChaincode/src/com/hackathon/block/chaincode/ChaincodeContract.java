b708621e4b2112874171f18fe1334180d66cb64b
amine <amine@DESKTOP-PIMQCD1>
2017-01-29 09:29:07 +0100

    Some comments

--- BlockChaincode/src/com/hackathon/block/chaincode/ChaincodeContract.java
+++ BlockChaincode/src/com/hackathon/block/chaincode/ChaincodeContract.java
@@ -1,5 +1,12 @@
 package com.hackathon.block.chaincode;
 
+/**
+ * Smart Contract
+ * @author B'Lock Team
+ *
+ */
 public class ChaincodeContract {
-
+	
+	
+	
 }
--- BlockChaincode/src/com/hackathon/block/chaincode/ChaincodeService.java
+++ BlockChaincode/src/com/hackathon/block/chaincode/ChaincodeService.java
@@ -1,5 +1,10 @@
 package com.hackathon.block.chaincode;
 
+/**
+ * Exchange service with the web app
+ * @author B'Lock Team
+ *
+ */
 public class ChaincodeService {
 
 }
