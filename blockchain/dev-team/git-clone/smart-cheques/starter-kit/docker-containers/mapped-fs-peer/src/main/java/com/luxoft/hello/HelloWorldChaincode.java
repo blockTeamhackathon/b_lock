ddc8907a836a64785f2a062d772764ea44050d0d
Richard Ulrich <richi@paraeasy.ch>
2017-01-29 12:17:19 +0100

    changes from Jean Luc

--- blockchain/dev-team/git-clone/smart-cheques/starter-kit/docker-containers/mapped-fs-peer/src/main/java/com/luxoft/hello/HelloWorldChaincode.java
+++ blockchain/dev-team/git-clone/smart-cheques/starter-kit/docker-containers/mapped-fs-peer/src/main/java/com/luxoft/hello/HelloWorldChaincode.java
@@ -1,15 +1,33 @@
 package com.luxoft.hello;
 
+import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 import org.hyperledger.java.shim.ChaincodeBase;
 import org.hyperledger.java.shim.ChaincodeStub;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+// import org.bouncycastle.crypto.BlockCipher;
+// import org.bouncycastle.crypto.BufferedBlockCipher;
+// import org.bouncycastle.crypto.engines.AESEngine;
+// import org.bouncycastle.crypto.engines.DESEngine;
+// import org.bouncycastle.crypto.modes.CBCBlockCipher;
+// import org.bouncycastle.crypto.paddings.PaddedBufferedBlockCipher;
+// import org.bouncycastle.crypto.params.KeyParameter;
+// import org.hyperledger.common.*;
 
 
 /**
  * b_lock fighting supply chain fraud
  */
+
+
 public class HelloWorldChaincode extends ChaincodeBase {
 
   private static final String CHAINCODE_NAME = "HelloWorldChaincode";
@@ -37,10 +55,12 @@ public class HelloWorldChaincode extends ChaincodeBase {
   @Override
   public String query(ChaincodeStub chaincodeStub, String function, String[] args) {
     switch(function) {
-        case "getTransaction":
-            return getTransaction(args);
+        case "getLastTransactionfromTx":
+            return getLastTransactionFromTx(chaincodeStub, args[0]);
         case "getTransactionCount":
             return Integer.toString(getTransactionCount(chaincodeStub, args[0]));
+        case "getLastTxfromLockId":
+        	return getLastTransactionFromLockId(chaincodeStub, args[0]);
         default:
             return "No matching case for function:" + function;
     }
@@ -50,15 +70,43 @@ public class HelloWorldChaincode extends ChaincodeBase {
   	
   	insertTransaction(stub, args);
   	
-      return "Insert " + args[0];
+    return "Insert " + args[0];
   }
 
     public String lock(String[] args){
         return "closing";
     }
 
-    public String getTransaction(String[] args){
-        return "get transaction";
+    public String getLastTransactionFromTx(ChaincodeStub stub, String transactionId){
+    	
+    	int tx_count = getTransactionCount(stub, transactionId);
+    	if(tx_count >= 0)
+    	{
+    		String last_transactionId = transactionId + "_" + (tx_count-1);
+    		return stub.getState(last_transactionId);
+    	}
+    	return null;
+    	
+    }
+    
+    public String getLastTransactionFromLockId(ChaincodeStub stub, String lockId){
+    	
+    	String transactionId = stub.getState(lockId);
+		
+		System.out.println("TxId from LockId : Ledger[" + lockId + "] = " + transactionId);
+		if(!transactionId.isEmpty())
+		{
+			int tx_count = getTransactionCount(stub, transactionId);
+			if(tx_count >= 0)
+			{
+				String last_transactionId = transactionId + "_" + (tx_count-1);
+				System.out.println("last_transactionId = " + last_transactionId);
+				System.out.println("last_transactionId stub" + stub.getState(last_transactionId));
+				return stub.getState(last_transactionId);
+			}
+		}
+    	return null;
+    	
     }
 
     public String unlock(String[] args){
@@ -94,6 +142,7 @@ public class HelloWorldChaincode extends ChaincodeBase {
     public String insertTransaction(ChaincodeStub stub, String[] args){
     	
     	String transactionId = args[0];
+    	String jsonContent = args[1];
     	String state = stub.getState(transactionId);
     	int transaction_count = 0;
     	if(state == null)
@@ -109,17 +158,57 @@ public class HelloWorldChaincode extends ChaincodeBase {
 				return "{\"Error\":\"Expecting integer value for asset holding of "+state+" \"}";		
 			}
     	}
+    	System.out.println("JSONCONTENT");
+    	System.out.println(jsonContent);
+    	
+    	// reading the key from the json
+    	JsonParser parser = new JsonParser();
+    	JsonElement element = parser.parse(jsonContent);
+    	JsonObject obj = element.getAsJsonObject();
+    	
+    	JsonElement je2 = obj.get("lockId");
+    	String lockId = je2.getAsString();
+    	String lock_state = stub.getState(lockId);
+		System.out.println("lock_state");
+    	System.out.println(lock_state);
+    	if(lock_state == null || lock_state.isEmpty())
+    	{
+    		stub.putState(lockId, transactionId);
+			System.out.println("Write key into ledger. Key is : " + lockId);
+			System.out.println("Write tx into ledger. Key is : " + transactionId);
+    	}
     	
     	String numbered_transactionId = transactionId + "_" + transaction_count;
     	transaction_count++;
-    	stub.putState(numbered_transactionId, args[1]);
+    	stub.putState(numbered_transactionId, jsonContent);
     	stub.delState(transactionId);
     	stub.putState(transactionId, Integer.toString(transaction_count));
 
 
         return null;
     }
+    	public static String insertTransaction_test( String[] args){
+    	
+    	String transactionId = args[0];
+    	String jsonContent = args[1];
+  
+    	System.out.println("JSONCONTENT");
+    	System.out.println(jsonContent);
+    	
+    	// reading the key from the json
+    	JsonParser parser = new JsonParser();
 
+    	JsonElement element = parser.parse(jsonContent);
+    	JsonObject obj = element.getAsJsonObject();
+    	
+    	JsonElement je2 = obj.get("lockId");
+    	String lockId = je2.getAsString();
+    	
+        return null;
+    }
+
+    
+    
   @Override
   public String getChaincodeID() {
     return CHAINCODE_NAME;
@@ -127,5 +216,7 @@ public class HelloWorldChaincode extends ChaincodeBase {
 
   public static void main(String[] args) {
     new HelloWorldChaincode().start(args);
+	  //insertTransaction_test(args);
+	  
   }
 }
--- scripts/startTransaction.sh
+++ scripts/startTransaction.sh
@@ -3,3 +3,30 @@
 docker exec -it starter peer chaincode invoke -l java -n  HelloWorldChaincode -c '{"Args":["startTransaction", "1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa", "{\blah\"}"]}'
 sleep 1
 docker exec -it starter peer chaincode query -l java -n  HelloWorldChaincode -c '{"Args":["getTransactionCount", "1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa"]}'
+
+
+
+peer chaincode invoke -l java -n  HelloWorldChaincode -c '{"Args":["startTransaction", "5555zP1eP5QGefi2DMPTfTL5SLmv7DivfNa", "{\"id\":\"341zP1eP5QGefi2DMPTfTL5SLmv7DivfNa\",\"state\":\"started\",\"lockId\":\"lock_0\",\"usersId\":[\"user_0\",\"user_1\",\"user_2\"]}"]}'
+peer chaincode invoke -l java -n  HelloWorldChaincode -c '{"Args":["startTransaction", "6666P1eP5QGefi2DMPTfTL5SLmv7DivfNa", "{\"id\":\"341zP1eP5QGefi2DMPTfTL5SLmv7DivfNa\",\"state\":\"started\",\"lockId\":\"lock_1\",\"usersId\":[\"user_0\",\"user_1\",\"user_2\"]}"]}'
+
+peer chaincode invoke -l java -n  HelloWorldChaincode -c '{"Args":["startTransaction", "5555zP1eP5QGefi2DMPTfTL5SLmv7DivfNa", "{\"id\":\"341zP1eP5QGefi2DMPTfTL5SLmv7DivfNa\",\"state\":\"in progress\",\"lockId\":\"lock_0\",\"usersId\":[\"user_0\",\"user_1\",\"user_2\"]}"]}'
+peer chaincode invoke -l java -n  HelloWorldChaincode -c '{"Args":["startTransaction", "6666P1eP5QGefi2DMPTfTL5SLmv7DivfNa", "{\"id\":\"341zP1eP5QGefi2DMPTfTL5SLmv7DivfNa\",\"state\":\"in progress\",\"lockId\":\"lock_1\",\"usersId\":[\"user_0\",\"user_1\",\"user_2\"]}"]}'
+
+peer chaincode invoke -l java -n  HelloWorldChaincode -c '{"Args":["startTransaction", "5555zP1eP5QGefi2DMPTfTL5SLmv7DivfNa", "{\"id\":\"341zP1eP5QGefi2DMPTfTL5SLmv7DivfNa\",\"state\":\"closed\",\"lockId\":\"lock_0\",\"usersId\":[\"user_0\",\"user_1\",\"user_2\"]}"]}'
+peer chaincode invoke -l java -n  HelloWorldChaincode -c '{"Args":["startTransaction", "6666P1eP5QGefi2DMPTfTL5SLmv7DivfNa", "{\"id\":\"341zP1eP5QGefi2DMPTfTL5SLmv7DivfNa\",\"state\":\"closed\",\"lockId\":\"lock_1\",\"usersId\":[\"user_0\",\"user_1\",\"user_2\"]}"]}'
+
+
+peer chaincode query -l java -n  HelloWorldChaincode -c '{"Args":["getTransactionCount", "5555zP1eP5QGefi2DMPTfTL5SLmv7DivfNa"]}'
+
+peer chaincode query -l java -n  HelloWorldChaincode -c '{"Args":["getTransactionCount", "6666P1eP5QGefi2DMPTfTL5SLmv7DivfNa"]}'
+
+peer chaincode query -l java -n  HelloWorldChaincode -c '{"Args":["getLastTransactionfromTx", "5555zP1eP5QGefi2DMPTfTL5SLmv7DivfNa"]}'
+
+peer chaincode query -l java -n  HelloWorldChaincode -c '{"Args":["getLastTransactionfromTx", "6666P1eP5QGefi2DMPTfTL5SLmv7DivfNa"]}'
+
+peer chaincode query -l java -n  HelloWorldChaincode -c '{"Args":["getLastTxfromLockId", "lock_0"]}'
+
+peer chaincode query -l java -n  HelloWorldChaincode -c '{"Args":["getLastTxfromLockId", "lock_1"]}'
+
+
+
